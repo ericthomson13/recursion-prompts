782c39c5d8c4f094ba9f64b8ab20d19694081412
et13 <eric.thomson13@gmail.com>
2019-12-02 10:42:19 -0500

    Worked through Problems 1-3, 5
    Working through problems need to work on how to test to ensure they're working.

--- src/recursion.js
+++ src/recursion.js
@@ -7,11 +7,27 @@
 // Example: 5! = 5 x 4 x 3 x 2 x 1 = 120
 // factorial(5); // 120
 var factorial = function(n) {
+    if (n < 0) {
+        return -1;
+    }
+    if (n === 0) {
+        return 1;
+    }
+    if (n > 0) {
+        return n * factorial(n - 1);
+    }
 };
 
 // 2. Compute the sum of an array of integers.
 // sum([1,2,3,4,5,6]); // 21
 var sum = function(array) {
+    var index = array.length - 1;
+    if (index === -1) {
+        return 0;
+    }
+    if (index >= 0) {
+        return array[index] + sum(array[index - 1]);
+    }
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
@@ -27,6 +43,15 @@ var isEven = function(n) {
 // sumBelow(10); // 45
 // sumBelow(7); // 21
 var sumBelow = function(n) {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    if (n > 1) {
+        return n + sumBelow(n -1);
+    }
 };
 
 // 6. Get the integers within a range (x, y).
