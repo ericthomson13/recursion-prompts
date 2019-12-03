daa7de98093417e552c8205f88e56af12888208e
et13 <eric.thomson13@gmail.com>
2019-12-02 19:17:09 -0500

    Notes on testing isEven
    Added recursive call but testing didn't work need to work on more to see what is going wrong

--- src/recursion.js
+++ src/recursion.js
@@ -50,14 +50,15 @@ var arraySum = function(array) {
 
 // 4. Check if a number is even.
 var isEven = function(n) {
-   if (n === 0) {
-       return true;
-   } else if (n === 1) {
+   if (n === 1) {
        return false;
+   } else if (n === 2) {
+       return true;
    } else {
        isEven(n-2);
    }
 };
+// TESTING IS NOT WORKING NEED TO COME BACK AND REWORK *************************************************
 
 // 5. Sum all integers below a given integer.
 // sumBelow(10); // 45
