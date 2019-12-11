6a571b49b57ba4d12658bdbf2b4f86750580dfcd
ericthomson13 <eric.thomson13@gmail.com>
2019-12-10 22:16:43 -0700

    make factorial work for 1

--- src/recursion.js
+++ src/recursion.js
@@ -10,8 +10,8 @@ var factorial = function(n) {
     if (n < 0) {
         return null;
     }
-    if (n === 0) {
-        return 0;
+    if (n  === 1) {
+        return ;
     }
     if (n > 1) {
         return factorial(n -1);
